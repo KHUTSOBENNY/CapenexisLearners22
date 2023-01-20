401e5d90b171291fe1c446a86ed96581da144889
KHUTSOBENNY <bennykhutso4@gmail.com>
2023-01-20 08:35:19 +0200

    Adding codes and folders

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -2,5 +2,10 @@
 {
 	public class Courses
 	{
+		public int Id { get; set; }
+		public string? Lecture { get; set; }
+		public string? CourseName { get; set; }
+		public string? Duration { get; set; }
+		public decimal Price { get; set; }
 	}
 }
